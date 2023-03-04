287126926715be7a7ed009b7ab73478292beef18
Praveen MP <praveen_mp@live.com>
2023-03-04 22:04:46 +0530

    Added Amazon link and also added Percentage calcultor

---
+++ stockaveragecalculator/script.js
@@ -0,0 +1,148 @@
+function unitChanged(id, error) {
+    const unit1 = document.getElementById("units1").value;
+    const price1 = document.getElementById("Price1").value;
+    const unit2 = document.getElementById("units2").value;
+    const price2 = document.getElementById("Price2").value;
+
+    if(unit1 !='' && price1 !='' && unit2 !='' && price2 != '') {
+        getAverage();
+    }
+
+
+    if (document.getElementById(id).value != '') {
+      document.getElementById(error).innerHTML = '';
+      return;
+    } else {
+      document.getElementById(error).innerHTML = 'Field is Required';
+    }
+   
+  }
+
+
+  function getAverage() {
+
+    if (document.getElementById("units1").value == '') {
+      document.getElementById("error1").innerHTML = "Units required";
+      return;
+    } else if (document.getElementById("Price1").value == '') {
+      document.getElementById("error2").innerHTML = "Price required";
+      return;
+    } else if (document.getElementById("units2").value == '') {
+      document.getElementById("error3").innerHTML = "Units required";
+      return;
+    } else if (document.getElementById("Price2").value == '') {
+      document.getElementById("error4").innerHTML = "Price required";
+      return;
+    }
+
+    var unit1 = document.getElementById("units1").value;
+    var price1 = document.getElementById("Price1").value;
+    var unit2 = document.getElementById("units2").value;
+    var price2 = document.getElementById("Price2").value;
+
+    var totalAmount = ((unit1 * price1) + (unit2 * price2)).toLocaleString('en-IN');
+    var totalUnits = +unit1 + +unit2;
+    var averagePrice = (((unit1 * price1) + (unit2 * price2)) / totalUnits).toLocaleString('en-IN');
+    document.getElementById("investedamount1").innerHTML ="<span>Amount invested on 1st buy: </span>  <span class='unit-amount'>" + (unit1 * price1).toLocaleString('en-IN') + "</span>";
+    document.getElementById("investedamount2").innerHTML ="<span>Amount invested on 2nd buy: </span> <span class='unit-amount'>" + (unit2 * price2).toLocaleString('en-IN') + "</span>";
+    document.getElementById("result").innerHTML = "<span>Total units </span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<bold>"
+      + totalUnits + "</bold><br/><br/><span>Average Price</span> &nbsp;&nbsp;&nbsp;<bold>" + averagePrice + "</bold>" 
+      + "<br/><br/><span>Total Amount</span> &nbsp;&nbsp;&nbsp;<bold>"
+      + totalAmount + "</bold>";
+  }
+  function clearFields() {
+    document.getElementById("units1").value = '';
+    document.getElementById("Price1").value = '';
+    document.getElementById("units2").value = '';
+    document.getElementById("Price2").value = '';
+    document.getElementById("result").innerHTML = '';
+    document.getElementById("investedamount1").innerHTML = '';
+    document.getElementById("investedamount2").innerHTML = '';
+  }
+
+
+  function activeSection(value) {
+      switch(value) {
+          case 'ac':
+              document.getElementById("average-calculator").classList.remove("hide"); 
+              document.getElementById("average-calculator").classList.add("active");
+
+              document.getElementById("sip-calculator").classList.remove("active");
+              document.getElementById("sip-calculator").classList.add("hide");
+
+              document.getElementById("percentage-calculator").classList.remove("active");
+              document.getElementById("percentage-calculator").classList.add("hide");
+
+
+              document.getElementById("ac").classList.add("active");
+              document.getElementById("sc").classList.remove("active");
+              document.getElementById("pc").classList.remove("active");
+          break;
+          case 'sc': 
+              document.getElementById("sip-calculator").classList.remove("hide");
+              document.getElementById("sip-calculator").classList.add("active");
+
+              document.getElementById("average-calculator").classList.remove("active");
+              document.getElementById("average-calculator").classList.add("hide");
+
+              document.getElementById("percentage-calculator").classList.remove("active");
+              document.getElementById("percentage-calculator").classList.add("hide");
+
+              document.getElementById("sc").classList.add("active");
+              document.getElementById("ac").classList.remove("active");
+              document.getElementById("pc").classList.remove("active");
+          break;
+          case 'pc': 
+          document.getElementById("percentage-calculator").classList.add("active");
+          document.getElementById("percentage-calculator").classList.remove("hide");
+
+          document.getElementById("sip-calculator").classList.remove("active");
+          document.getElementById("sip-calculator").classList.add("hide");
+
+          document.getElementById("average-calculator").classList.remove("active");
+          document.getElementById("average-calculator").classList.add("hide");
+
+          document.getElementById("pc").classList.add("active");
+          document.getElementById("ac").classList.remove("active");
+          document.getElementById("sc").classList.remove("active");
+      break;
+          default: break;
+      }
+  }
+
+  function sipCalculator() {
+      var investment = parseInt(document.getElementById("investment").value); 
+      var rateOfInterest = parseInt(document.getElementById("rateOfInterest").value);
+      var years = parseInt(document.getElementById("years").value);
+      var monthlyRate = rateOfInterest/12/100;	
+      var months= years*12;
+      var totalGain = (investment *(Math.pow((1 + monthlyRate), months) - 1)/monthlyRate *(1+monthlyRate));	
+      if(investment && rateOfInterest && years) {
+          document.getElementById("im").innerHTML = (investment * months).toLocaleString('en-IN');	
+          document.getElementById("tm").innerHTML = totalGain.toLocaleString('en-IN');
+          document.getElementById("gm").innerHTML = (totalGain - (investment * months)).toLocaleString('en-IN') ;
+      }
+      }
+
+function clearFields1() {
+    document.getElementById("investment").value = '';
+    document.getElementById("rateOfInterest").value = '';
+    document.getElementById("years").value = '';
+    document.getElementById("tm").innerHTML = '0';
+    document.getElementById("im").innerHTML = '0';
+    document.getElementById("gm").innerHTML = '0';
+  }
+
+  function checkPercentage() {
+	let num = Number(document.getElementById("percentageNumber").value);
+	let percent = Number(document.getElementById("percentage").value);
+	let result = document.getElementById("percentageResult");
+
+	if (num == '' || percent == '' || num == 0 || percent == 0) {
+		return;
+	}
+
+	let finalNum = (num/100) * percent;
+
+	result.innerHTML = num + " % " + percent + " is = " + finalNum;
+}
