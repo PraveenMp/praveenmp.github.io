5534fb55737e14c3c12206971134d49c03b5cdfa
Praveena Paramashiva <Praveena.Paramashiva@pickles.com.au>
2023-12-22 22:38:21 +0530

    Remove IPad and Ipod

--- stockaveragecalculator/script.js
+++ stockaveragecalculator/script.js
@@ -246,7 +246,7 @@ document.getElementById('toggle-btn').addEventListener('click', function () {
 });
 
 function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return /Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
 // Example usage:
