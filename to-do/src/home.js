92d58c0d657c53b259306c1f7088d0054bb86d8f
Praveena Paramashiva <Praveena.Paramashiva@pickles.com.au>
2024-03-05 21:40:31 +0530

    tod-do app updated

--- to-do/src/home.js
+++ to-do/src/home.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./components/login/login";
 
@@ -7,14 +7,6 @@ function Home() {
     return (
     <>
     <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-        </ul>
     </nav>
         <Routes>
         <Route path="/" element={<App />}></Route>
